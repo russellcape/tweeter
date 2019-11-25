df590dff30c52fa4d539793021a21cda8de28953
Russell Cape <russellcape98@gmail.com>
2019-11-25 20:42:05 +0000

    Counter in working order

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $("form textarea").keyup(function() {
+  $("form textarea").val('').keyup(function() {
     $("form span.counter").text(140 - $(this).val().length);
     if ($(this).val().length > 140) {
     $("form span.counter").addClass("red")
